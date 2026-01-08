ea8de95c87ace30fca7b951505b38ba0e599c758
David Hassell <david.hassell@ncas.ac.uk>
2026-01-08 13:06:31 +0000

    remove legacy XML files

--- cfunits/test/test_Units.py
+++ cfunits/test/test_Units.py
@@ -3,7 +3,7 @@ import math
 import os
 import unittest
 
-import numpy
+import numpy as np
 from dask.base import tokenize
 
 faulthandler.enable()  # to debug seg faults and timeouts
@@ -149,77 +149,78 @@ class UnitsTest(unittest.TestCase):
         )
 
         x = Units.conform([360], Units("second"), Units("minute"))
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertTrue(numpy.allclose(x, 6))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertTrue(np.allclose(x, 6))
 
         x = Units.conform((360, 720), Units("second"), Units("minute"))
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertTrue(numpy.allclose(x, [6, 12]))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertTrue(np.allclose(x, [6, 12]))
 
         x = Units.conform([360.0, 720.0], Units("second"), Units("minute"))
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertTrue(numpy.allclose(x, [6, 12]))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertTrue(np.allclose(x, [6, 12]))
 
         x = Units.conform([[360, 720]], Units("second"), Units("minute"))
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertTrue(numpy.allclose(x, [[6, 12]]))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertTrue(np.allclose(x, [[6, 12]]))
 
-        v = numpy.array([360.0, 720.0], dtype=numpy.dtype("float64"))
+        v = np.array([360.0, 720.0], dtype=np.dtype("float64"))
         x = Units.conform(v, Units("second"), Units("minute"))
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertEqual(x.dtype, numpy.dtype("float64"))
-        self.assertTrue(numpy.allclose(x, [6, 12]), x)
+        self.assertIsInstance(x, np.ndarray)
+        self.assertEqual(x.dtype, np.dtype("float64"))
+        self.assertTrue(np.allclose(x, [6, 12]), x)
 
-        v = numpy.array([360, 720], dtype=numpy.dtype("int64"))
+        v = np.array([360, 720], dtype=np.dtype("int64"))
         x = Units.conform(v, Units("second"), Units("minute"), inplace=True)
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertEqual(x.dtype, numpy.dtype("float64"))
-        self.assertTrue(numpy.allclose(x, [6, 12]))
-        self.assertTrue(numpy.allclose(x, v))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertEqual(x.dtype, np.dtype("float64"))
+        self.assertTrue(np.allclose(x, [6, 12]))
+        self.assertTrue(np.allclose(x, v))
 
-        v = numpy.array([360, 720], dtype=numpy.dtype("int32"))
+        v = np.array([360, 720], dtype=np.dtype("int32"))
         x = Units.conform(v, Units("second"), Units("minute"), inplace=True)
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertEqual(x.dtype, numpy.dtype("float32"))
-        self.assertTrue(numpy.allclose(x, [6, 12]))
-        self.assertTrue(numpy.allclose(x, v))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertEqual(x.dtype, np.dtype("float32"))
+        self.assertTrue(np.allclose(x, [6, 12]))
+        self.assertTrue(np.allclose(x, v))
 
-        for i in range(24):
-            v = numpy.array([], dtype=numpy.sctypeDict[i])
+        for t in np.sctypeDict:
+            v = np.array([], dtype=t)
             if v.dtype.kind == "i":
-                v = numpy.array([60, 120], dtype=numpy.sctypeDict[i])
+                v = np.array([60, 120], dtype=t)
                 check_dtype = v.dtype.str
                 x = Units.conform(
                     v, Units("second"), Units("minute"), inplace=True
                 )
-                self.assertIsInstance(x, numpy.ndarray)
+                self.assertIsInstance(x, np.ndarray)
                 if check_dtype[-1] in ["1", "2"]:
                     # no inplace converting possible
                     self.assertEqual(x.dtype.itemsize, 4)
                 else:
                     self.assertEqual(x.dtype.str, v.dtype.str)
-                    self.assertTrue(numpy.allclose(x, v))
-                self.assertTrue(numpy.allclose(x, [1, 2]))
+                    self.assertTrue(np.allclose(x, v))
+
+                self.assertTrue(np.allclose(x, [1, 2]))
 
         x = Units.conform(35, Units("degrees_C"), Units("degrees_F"))
         self.assertIsInstance(x, float)
-        self.assertTrue(numpy.allclose(x, 95))
+        self.assertTrue(np.allclose(x, 95))
 
         x = Units.conform([35], Units("degrees_C"), Units("degrees_F"))
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertTrue(numpy.allclose(x, 95))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertTrue(np.allclose(x, 95))
 
         x = Units.conform(
             35, Units("degrees_C"), Units("degrees_F"), inplace=True
         )
         self.assertIsInstance(x, float)
-        self.assertTrue(numpy.allclose(x, 95))
+        self.assertTrue(np.allclose(x, 95))
 
         x = Units.conform(
             [35], Units("degrees_C"), Units("degrees_F"), inplace=True
         )
-        self.assertIsInstance(x, numpy.ndarray)
-        self.assertTrue(numpy.allclose(x, 95))
+        self.assertIsInstance(x, np.ndarray)
+        self.assertTrue(np.allclose(x, 95))
 
         with self.assertRaises(ValueError):
             Units.conform(1, Units("m"), Units("second"))
